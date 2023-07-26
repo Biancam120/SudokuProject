79d82b2de7f63202b88e2e4d002922334d596bda
Biancam120 <139014057+Biancam120@users.noreply.github.com>
2023-07-26 18:48:23 -0400

    added to functions in class Sudoku_generator

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,34 @@
 import pygame
 from sudoku_generator import *
+import pygame
 
-# width = 900
-# height = 900  # each square will be 100
-# BG_COLOR =
-# square_size = 300
-# line_width = 5
+pygame.init()
+
+width = 900
+height = 900  # each square will be 100
+BG_COLOR = (191, 239, 255)
+square_size = 300
+line_width = 5
+row_length = 9
+col_length = 9
+screen = pygame.display.set_mode((width, height))
+line_color = (0, 0, 0)
+NUM_FONT = 100
+number_font = pygame.font.Font(None, NUM_FONT)
+number_color = (0, 0, 0)
 
 def main():  # contains code to create different screens of project
 	pygame.init()
 	pygame.display.set_caption("")
 	screen = pygame.display.set_mode((width, height))  # created black pop-up window
 	screen.fill(BG_COLOR)  # changes background color
+
+	for i in range(1, row_length):  # 8 horizonatal lines, 9 rows
+		pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size), line_width)  # assuming screen 900 x 900
+
+	for i in range(1, col_length):
+		pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size), height)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,48 +1,59 @@
-import pygame
 
-pygame.init()
-
-width = 900
-height = 900  # each square will be 100
-BG_COLOR = (191, 239, 255)
-square_size = 300
-line_width = 5
-row_length = 9
-col_length = 9
-screen = pygame.display.set_mode((width, height))
-line_color = (0, 0, 0)
-NUM_FONT = 100
-number_font = pygame.font.Font(None, NUM_FONT)
-number_color = (0, 0, 0)
 
 class SudokuGenerator:
 	def __init__(self, row_length, removed_cells):
-		self.row_length = row_length  # always 9
-		self.removed_cells = removed_cells  # varies depending on difficulty (easy = 30, med = 40, hard = 50)
+		self.row_length = 9  # always 9
+		self.removed_cells = removed_cells  # depending on difficulty (easy = 30, med = 40, hard = 50)
+		self.board = [[0 for i in range(row_length)] for j in range(row_length)] # prints 0 in each box in board
+		self.box_length = 3 # square root of the row
 
 	def get_board(self):  # returns 2D list of num which represents the board
-		pass
+		return self.board
 
 	def print_board(self):
-		pass
+		return self.board  # displays board to the console
 
 	def valid_in_row(self, row, num):
-		if self.row == num:  # determines if num is in given row of the board
-			return True
-		else:
-			return False
+		for col in range(self.row_length):  # looks through 9 col
+			if self.board[row][col] == num:  # determines if num is in given row of the board
+				return False
+			else:
+				return True
 
 	def valid_in_col(self, col, num):
-		if self.col == num:
-			return True
-		else:
-			return False
+		for row in range(self.row_length):  # looks through 9 rows
+			if self.board[row][col] == num: # determines if num is in given col
+				return False
+			else:
+				return True
 
 	def valid_in_box(self, row_start, col_start, num):
-		pass
+		# for row in range(row_start, row_start + 2):
+		# 	if self.board[row][col_start] == num: # when you fix the row you look at all the columns
+		# 		return False
+		# 	else:
+		# 		return True
+		# for col in range(col_start, col_start + 2):
+		# 	if self.board[row_start][col] == num:
+		# 		return False
+		# 	else:
+		# 		return True
+
+		for i in range(row_start, row_start + 2): # runs through the 3 rows in the box
+			if self.board[row_start][i] == num:  # fixes row and checks each column
+				return False
+			else:
+				return True
+
+		for i in range(col_start, col_start + 2): # runs through the 3 columns in the box
+			if self.board[i][col_start] == num: # fixes column and checks each row for number
+				return False
+			else:
+				return True
+
 
 	def is_valid(self, row, col, num):
-		pass
+
 
 	def fill_box(self, row_start, col_start):
 		pass
@@ -50,18 +61,48 @@ class SudokuGenerator:
 	def fill_diagonal(self):
 		pass
 
-	def fill_remaining(self, row, col):  # method is provided
-		pass
+	def fill_remaining(self, row, col):  # method was provided on Github, fills remaining cells of the board
+		if (col >= self.row_length and row < self.row_length - 1):
+			row += 1
+			col = 0
+		if row >= self.row_length and col >= self.row_length:
+			return True
+		if row < self.box_length:
+			if col < self.box_length:
+				col = self.box_length
+		elif row < self.row_length - self.box_length:
+			if col == int(row // self.box_length * self.box_length):
+				col += self.box_length
+		else:
+			if col == self.row_length - self.box_length:
+				row += 1
+				col = 0
+				if row >= self.row_length:
+					return True
 
-	def fill_values(self):  # method is provided
-		pass
+		for num in range(1, self.row_length + 1):
+			if self.is_valid(row, col, num):
+				self.board[row][col] = num
+				if self.fill_remaining(row, col + 1):
+					return True
+				self.board[row][col] = 0
+		return False  # True if board could be solved
 
-	def remove_cells(self):
-		pass
+	def fill_values(self):  # method was provided, constructs solution bby filling boxes
+		self.fill_diagonal()
+		self.fill_remaining(0, self.box_length)
+
+	def remove_cells(self):  # sets values to zero, called after solution is constructed
+		self.removed_cells = 0
 
 
-def generate_sudoku(size, removed):  # function outside class
-	pass
+def generate_sudoku(size, removed):  # function outside class, was provided
+	sudoku = SudokuGenerator(size, removed) # created a SudokuGenerator
+	sudoku.fill_values() # fills values and saves it as the solved state
+	board = sudoku.get_board()
+	sudoku.remove_cells() # removes cells depending on difficulty
+	board = sudoku.get_board()
+	return board  # returns 2D lists to make board with its solution
 
 
 class Cell:
@@ -91,11 +132,7 @@ class Board:
 		# screen = pygame.display.set_mode((width, height)) # displays pop up screen
 
 	def draw(self):
-		for i in range(1, row_length):  # 8 horizonatal lines, 9 rows
-			pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size), line_width)  # assuming screen 900 x 900
-
-		for i in range(1, col_length):
-			pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size), height)
+		pass
 
 	def select(self, row, col):
 		pass
