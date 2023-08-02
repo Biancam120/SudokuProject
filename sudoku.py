5e4ffd827bd18a8b6a4641201a153df9d7f7485c
Santy7543 <santy7543@gmail.com>
2023-08-01 21:44:16 -0400

    created start menu with different font sizes, created the buttons for difficulty options (i did not write the code for changing difficulty yet).
    I made a function that draws the lines instead of just having it in the main code so its more organized and I also changed the code a bit to have every 3rd line be thicker for visual clarity.

--- sudoku.py
+++ sudoku.py
@@ -17,27 +17,99 @@ number_font = pygame.font.Font(None, NUM_FONT)
 number_color = (0, 0, 0)
 
 
+def draw_menu_screen():
+    text_color = (0, 0, 0)
+    title_font = pygame.font.Font(None, 100)
+    select_font = pygame.font.Font(None, 75)
+    button_font = pygame.font.Font(None, 50)
+    button_color = (231, 223, 176)
+    screen.fill(BG_COLOR)
+
+    # welcome message code below
+    welcome_text = "Welcome to Sudoku"
+    welcome_surf = title_font.render(welcome_text, 0, text_color)
+    welcome_rect = welcome_surf.get_rect(center=(width // 2, height // 4 - 50))
+    screen.blit(welcome_surf, welcome_rect)
+
+    # game mode selection message below
+    mode_text = "Select a Game Mode:"
+    mode_surf = select_font.render(mode_text, 0, text_color)
+    mode_rect = mode_surf.get_rect(center=(width // 2, height - 400))
+    screen.blit(mode_surf, mode_rect)
+
+    # easy button code
+    easy_text = button_font.render("EASY", 0, text_color)
+    easy_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))  # creates surface that is 20 pixels larger all around the text
+    easy_surface.fill(button_color)
+    easy_surface.blit(easy_text, (10, 10))  # blits at (10, 10) so its in the middle of the surface box
+    easy_rectangle = easy_surface.get_rect(center=(width // 2 - 250, height - 200))
+    screen.blit(easy_surface, easy_rectangle)
+
+    # medium button code
+    medium_text = button_font.render("MEDIUM", 0, text_color)
+    medium_surface = pygame.Surface((medium_text.get_size()[0] + 20, medium_text.get_size()[1] + 20))
+    medium_surface.fill(button_color)
+    medium_surface.blit(medium_text, (10, 10))
+    medium_rectangle = medium_surface.get_rect(center=(width // 2, height - 200))
+    screen.blit(medium_surface, medium_rectangle)
+
+    # hard button code
+    hard_text = button_font.render("HARD", 0, text_color)
+    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surface.fill(button_color)
+    hard_surface.blit(hard_text, (10, 10))
+    hard_rectangle = hard_surface.get_rect(center=(width // 2 + 250, height - 200))
+    screen.blit(hard_surface, hard_rectangle)
+
+    while True:  # this keeps the user on this screen until they choose a difficulty, then returns to the main function
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_rectangle.collidepoint(event.pos):
+                    # set easy difficulty code here
+                    return
+                elif medium_rectangle.collidepoint(event.pos):
+                    # set medium difficulty code here
+                    return
+                elif hard_rectangle.collidepoint(event.pos):
+                    # hard difficulty code here
+                    return
+        pygame.display.update()
+
+
+def draw_lines():
+    for i in range(1, row_length):  # draws 8 horizontal lines, 9 rows
+        if i % 3 == 0:  # if statement to make every 3rd line thicker for visual purposes
+            pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size),
+                             line_width + 4)  # assuming screen 900 x 900
+        else:
+            pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size), line_width)
+
+    for i in range(1, col_length):  # draws 8 vertical lines
+        if i % 3 == 0:  # if statement to make every 3rd line thicker for visual purposes
+            pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size, height), line_width + 4)
+        else:
+            pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size, height), line_width)
+
+
 def main():  # contains code to create different screens of project
-	pygame.display.set_caption("")
-	screen.fill(BG_COLOR)  # changes background color
+    draw_menu_screen()
+    pygame.display.set_caption("Sudoku")
+    screen.fill(BG_COLOR)  # changes background color
+    draw_lines()
 
-	for i in range(1, row_length):  # draws 8 horizonatal lines, 9 rows
-		pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size), line_width)  # assuming screen 900 x 900
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()  # can close window
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:  # for selecting square?
+                x, y = event.pos
+                row, col = y // square_size, x // square_size
 
-	for i in range(1, col_length): # draws 8 vertical lines
-		pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size), height)
-
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()  # can close window
-				sys.exit()
-			if event.type ==pygame.MOUSEBUTTONDOWN:  # for selecting square?
-				x, y = event.pos
-				row, col = y // square_size, x // square_size
-
-		pygame.display.update()
+        pygame.display.update()
 
 
 if __name__ == '__main__':
-	main()
+    main()
